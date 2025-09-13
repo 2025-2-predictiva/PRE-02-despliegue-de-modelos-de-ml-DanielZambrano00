20b1b1baa574630356e0344b464f3aea08bda7de
DanielZambrano00 <danidzg2000@gmail.com>
2025-09-13 12:17:36 -0500

    Respuesta_PRE02

---
+++ homework/descriptivo.py
@@ -0,0 +1,33 @@
+#%%
+import pandas as pd  #  type: ignore
+
+df = pd.read_csv("../files/input/house_data.csv")
+
+features = df[
+    [
+        "bedrooms",
+        "bathrooms",
+        "sqft_living",
+        "sqft_lot",
+        "floors",
+        "waterfront",
+        "condition",
+    ]
+]
+
+target = df[["price"]]
+
+features.bathrooms.value_counts().sort_index().plot(kind="bar")
+
+
+
+
+features.sqft_living.plot(kind="hist", edgecolor="white", bins=20)
+
+
+
+
+
+
+target.plot(kind="hist", edgecolor="white", bins=10)
+# %%
